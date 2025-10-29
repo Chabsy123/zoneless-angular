77faac9d513328aa39479d42503b0372b5f8096d
Chukwu Sydney <chukwusydney061@gmail.com>
2025-10-29 15:25:57 +0100

    Show Data Fetching with Angular 20

--- src/app/scenario-component/scenario-component.ts
+++ src/app/scenario-component/scenario-component.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component,inject, signal} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { JsonPipe } from '@angular/common';
+
 
 @Component({
   selector: 'app-scenario-component',
-  imports: [],
+  imports: [JsonPipe],
   templateUrl: './scenario-component.html',
   styleUrl: './scenario-component.scss',
 })
 export class ScenarioComponent {
   // First scenario
   count = signal(0);
+
+  // Second scenario
+  private http = inject(HttpClient);
+ //  Convert Observable to a Signal
+  data = toSignal(this.http.get<any[]>('https://jsonplaceholder.typicode.com/posts'));
 }
